09b6c3eaa67d38f2c68a59a3ed32ea3e8f0417db
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-01-09 19:54:23 +0100

    feat: IMplement compression for uploaded images

--- app/views/admin/books.py
+++ app/views/admin/books.py
@@ -2,6 +2,7 @@ import os
 from os import path
 from uuid import uuid4
 
+from PIL import Image as PillowImage
 from flask import (
     Blueprint,
     render_template,
@@ -87,8 +88,26 @@ def add_book():
                 file_name = (
                     f"{str(uuid4())}{path.splitext(secure_filename(file.filename))[1]}"
                 )
-                images.append(Image(name=file_name))
-                file.save(f"{current_app.config['UPLOADS_FOLDER']}/{file_name}")
+                file_save_path = f"{current_app.config['UPLOADS_FOLDER']}/{file_name}"
+                file.save(file_save_path)
+                image = PillowImage.open(file_save_path)
+                # Crop the center of the image
+                crop_size = image.width if image.width < image.height else image.height
+                left = (image.width - crop_size) // 2
+                top = (image.height - crop_size) // 2
+                right = (image.width + crop_size) // 2
+                bottom = (image.height + crop_size) // 2
+                # crop and resize
+                cropped_img = image.crop((left, top, right, bottom)).resize((390, 390))
+                print(cropped_img.width, "X", cropped_img.height)
+                print(image.width, "X", image.height)
+                webp_name = f"{path.splitext(file_save_path)[0]}.webp"
+                cropped_img.save(webp_name, optimize=True)
+                try:
+                    os.remove(file_save_path)
+                except:
+                    print("could not remove", file_save_path)
+                images.append(Image(name=path.split(webp_name)[1]))
         book.title = form.title.data
         book.description = form.description.data
         book.original_price = form.original_price.data
